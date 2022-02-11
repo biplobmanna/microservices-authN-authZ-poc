bf7e7ea3bccc695e86b48c9275e79e002bf9a6af
biplobmanna <biplobmanna@outlook.com>
2022-02-11 07:52:29 +0530

    hello world express server setup
    Setting up a "Hello, World!" express server with docker &
    docker-compose. Had some issue with starting the server and the
    container in which it was deployed. Removed and re-created the container
    image, and it started working fine. No idea what the issue was, but I'm
    guessing it was to do with either/or:
    * Dockerfile build commands while creating the original image
    * The volumes linked to the container
    * The original image
    * Some arbitraty limitation of docker, that I have yet to learn

--- users/index.js
+++ users/index.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+const express = require("express");
+const express_session = require("express-session");
+
+const app = express();
+const port = 5055;
+const host = '0.0.0.0';
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Hello, World!");
+});
+
+// Starting the server
+app.listen(port, host, () => {
+    console.log(`Users serivces started...\nListening on port ${port}\n`);
+});
